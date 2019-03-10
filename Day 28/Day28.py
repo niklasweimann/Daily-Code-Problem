05ecaf6eaa15badf36870e499b7848bb65a18c2e
Niklas Weimann <nikweimann@gmail.com>
2019-03-10 16:28:33 +0100

    Day 28

--- Day 28/Day28.py
+++ Day 28/Day28.py
@@ -1,4 +1,6 @@
 import math as m
+
+
 def justifyText(wordList, k):
     listLen = len(wordList)
     # Build cost matrix
@@ -7,15 +9,15 @@ def justifyText(wordList, k):
     for i in range(0, listLen):
         cost[i][i] = k - len(wordList[i])
         for j in range(i+1, listLen):
-            cost[i][j] = cost[i][j-1] - len(wordList[j]) -1
-    
+            cost[i][j] = cost[i][j-1] - len(wordList[j]) - 1
+
     for i in range(0, listLen):
         for j in range(i, listLen):
             if cost[i][j] < 0:
                 cost[i][j] = m.inf
             else:
                 cost[i][j] = m.pow(cost[i][j], 2)
-    
+
     minCost = [0 for y in range(0, listLen)]
     result = [0 for y in range(0, listLen)]
 
@@ -28,15 +30,32 @@ def justifyText(wordList, k):
             if minCost[i] > minCost[j]+cost[i][j-1]:
                 minCost[i] = minCost[j]+cost[i][j-1]
                 result[i] = j
-    
+
     i = 0
     print("Minimum cost is " + str(minCost[0]))
     print("\n")
     builder = []
     while True:
         j = result[i]
-        for k in range(i, j):
-            builder.append(wordList[k] + " ")
+        line = []
+        for l in range(i, j):
+            line.append(wordList[l])
+
+        for p in range(0, len(line)):
+            if p is len(line)-1:
+                break
+            line[p] = line[p] + " "
+
+        spaces = k - len("".join(line))
+
+        for n in range(len(line)):
+            s = ""
+            for o in range(0, m.ceil(spaces / len(line))):
+                s = s + " "
+            line[n] = line[n] + s
+
+        builder.append("".join(line))
+        builder.append("    " + "# {0} extra space(s)".format(spaces))
         builder.append("\n")
         i = j
         if j >= listLen:
